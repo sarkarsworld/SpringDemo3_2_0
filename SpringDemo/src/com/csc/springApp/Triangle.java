0dcfa1c950264cd5453f5999f278b86addc689b1
sarkarsworld <sarkarsworld@gmail.com>
2018-04-02 18:21:51 +0530

    Add files via upload

--- SpringDemo/src/com/csc/springApp/Triangle.java
+++ SpringDemo/src/com/csc/springApp/Triangle.java
@@ -2,20 +2,42 @@ package com.csc.springApp;
 
 public class Triangle {
 
-	private String type;
-	
+	//private String type;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
 	public void draw() {
-		System.out.println(getType() + " Triangle Drawn.");
+		
+		System.out.println("PointA = " + getPointA().getX() + "," + getPointA().getY());
+		System.out.println("PointB = " + getPointB().getX() + "," + getPointB().getY());
+		System.out.println("PointC = " + getPointC().getX() + "," + getPointC().getY());
+	}
+
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 
-	public String getType() {
-		return type;
-	}
-
-
-	public void setType(String type) {
-		this.type = type;
-	}
+	
 }
